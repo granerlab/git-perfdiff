01e717868004bf2fa792c0a3cf371162a28796b4
Johannes Graner <johannes.graner@gmail.com>
2024-10-14 18:46:33 +0200

    Create command without consuming Args

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,49 @@
+use clap::Parser;
 use std::process::{Command, Stdio};
 use std::time::Instant;
 
+/// Measure performance of a program across git commits.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Program to run
+    #[arg(short, long)]
+    pub program: String,
+
+    /// Arguments to pass to program
+    #[arg(short, long)]
+    pub arg: Option<Vec<String>>,
+
+    /// Working directory for program execution
+    #[arg(short, long)]
+    pub working_dir: Option<String>,
+
+    /// Whether to show program output
+    #[arg(long, action)]
+    pub show_output: bool,
+}
+
 pub struct CommandConfig<'a> {
-    pub program: &'a String,
-    pub args: &'a Vec<String>,
+    pub program: &'a str,
+    pub args: &'a [String],
     pub working_dir: &'a Option<String>,
     pub show_output: bool,
+}
+
+impl<'a> From<&'a Args> for CommandConfig<'a> {
+    fn from(value: &'a Args) -> Self {
+        const EMPTY_ARGS: &[String] = &[];
+        let program_args = value
+            .arg
+            .as_ref()
+            .map_or(EMPTY_ARGS, |arg_vec| arg_vec.as_slice());
+        CommandConfig {
+            program: &value.program,
+            args: program_args,
+            working_dir: &value.working_dir,
+            show_output: value.show_output,
+        }
+    }
 }
 
 impl CommandConfig<'_> {
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
 use clap::Parser;
-use git_perfdiff::{record_runtime, CommandConfig};
-
-/// Measure performance of a program across git commits.
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Program to run
-    #[arg(short, long)]
-    program: String,
-
-    /// Arguments to pass to program
-    #[arg(short, long)]
-    arg: Option<Vec<String>>,
-
-    /// Working directory for program execution
-    #[arg(short, long)]
-    working_dir: Option<String>,
-
-    /// Whether to show program output
-    #[arg(long, action)]
-    show_output: bool,
-}
-
+use git_perfdiff::{record_runtime, Args, CommandConfig};
 fn main() {
     let args = Args::parse();
 
-    let command = CommandConfig {
-        program: &args.program,
-        args: &args.arg.unwrap_or(Vec::new()),
-        working_dir: &args.working_dir,
-        show_output: args.show_output,
-    };
+    let command = CommandConfig::from(&args);
 
     let measurement = record_runtime(&command);
     println!("Ran in {measurement} seconds.");
