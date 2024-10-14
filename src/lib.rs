8a2ff689f24282d11aeceeeaae9c10aab5af9609
Johannes Graner <johannes.graner@gmail.com>
2024-10-14 20:12:43 +0200

    Validate commands before executing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
+use std::marker::PhantomData;
+use std::path::Path;
 use std::process::{Command, Stdio};
 use std::time::Instant;
+use which::which;
 
 /// Measure performance of a program across git commits.
 #[derive(Parser, Debug)]
@@ -23,14 +26,34 @@ pub struct Args {
     pub show_output: bool,
 }
 
-pub struct CommandConfig<'a> {
+pub trait CommandState {}
+pub struct NotValidated;
+pub struct Validated;
+
+impl CommandState for NotValidated {}
+impl CommandState for Validated {}
+
+pub struct CommandConfig<'a, S: CommandState> {
     pub program: &'a str,
     pub args: &'a [String],
     pub working_dir: &'a Option<String>,
     pub show_output: bool,
+    _marker: PhantomData<S>,
 }
 
-impl<'a> From<&'a Args> for CommandConfig<'a> {
+impl<'a, S: CommandState> CommandConfig<'a, S> {
+    const fn transition<N: CommandState>(self) -> CommandConfig<'a, N> {
+        CommandConfig {
+            program: self.program,
+            args: self.args,
+            working_dir: self.working_dir,
+            show_output: self.show_output,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a> From<&'a Args> for CommandConfig<'a, NotValidated> {
     fn from(value: &'a Args) -> Self {
         const EMPTY_ARGS: &[String] = &[];
         let program_args = value
@@ -42,11 +65,33 @@ impl<'a> From<&'a Args> for CommandConfig<'a> {
             args: program_args,
             working_dir: &value.working_dir,
             show_output: value.show_output,
+            _marker: PhantomData,
         }
     }
 }
 
-impl CommandConfig<'_> {
+#[derive(Debug)]
+pub enum CommandValidationFailure {
+    CommandNotFound,
+    WorkingDirNotFound,
+}
+
+impl<'a> CommandConfig<'a, NotValidated> {
+    pub fn validate(self) -> Result<CommandConfig<'a, Validated>, CommandValidationFailure> {
+        if which(self.program).is_err() {
+            return Err(CommandValidationFailure::CommandNotFound);
+        }
+        match self.working_dir {
+            // Follow symlinks and make sure path exists.
+            Some(path) if !Path::new(path).try_exists().unwrap_or(false) => {
+                Err(CommandValidationFailure::WorkingDirNotFound)
+            }
+            _ => Ok(self.transition()),
+        }
+    }
+}
+
+impl CommandConfig<'_, Validated> {
     #[must_use]
     pub fn to_command(&self) -> Command {
         let mut command = Command::new(self.program);
@@ -62,7 +107,7 @@ impl CommandConfig<'_> {
 }
 
 #[must_use]
-pub fn record_runtime(command: &CommandConfig) -> f64 {
+pub fn record_runtime(command: &CommandConfig<Validated>) -> f64 {
     let mut invocation = command.to_command();
 
     let timer = Instant::now();
