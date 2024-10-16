8c2de758556298b01de99450452b54ad955932a1
Johannes Graner <johannes.graner@gmail.com>
2024-10-16 19:05:34 +0200

    Checkout reference in git repository

---
+++ src/git.rs
@@ -0,0 +1,33 @@
+use git2::Repository;
+use std::path::PathBuf;
+
+/// Git repository context. Wraps the `git2::Repository` type.
+pub struct Context {
+    /// The wrapped repository.
+    pub repo: Repository,
+    /// Path to the git repository in the file system.
+    pub path: PathBuf,
+}
+
+impl Context {
+    /// Checkout a git reference (SHA, branch name, tag).
+    ///
+    /// # Errors
+    ///
+    /// Forwards any errors arising from `git2`.
+    pub fn checkout(&self, reference: &str) -> Result<(), git2::Error> {
+        let repo = &self.repo;
+        let (object, git_reference) = repo.revparse_ext(reference)?;
+        repo.checkout_tree(&object, None)?;
+        git_reference.map_or_else(
+            || repo.set_head_detached(object.id()),
+            |gref| {
+                let ref_name = gref
+                    .name()
+                    .ok_or_else(|| git2::Error::from_str("Reference name is not valid UTF-8"))?;
+                repo.set_head(ref_name)
+            },
+        )?;
+        Ok(())
+    }
+}
