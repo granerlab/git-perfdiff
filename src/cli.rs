48d2a65e3d6c03cd26087d2e9c54350df95d208e
Johannes Graner <johannes.graner@gmail.com>
2024-10-21 13:20:11 +0200

    Fix typos

--- src/cli.rs
+++ src/cli.rs
@@ -29,12 +29,12 @@ pub struct Args {
 
     /// Base commit in comparison
     // TODO: Default to branch split, or root commit.
-    // TOOD: Should this be an `OsString`?
+    // TODO: Should this be an `OsString`?
     #[arg()]
     pub base: Option<String>,
 
     /// Head commit in comparison
-    // TODO: Default to
+    // TODO: Default to current HEAD.
     #[arg()]
     pub head: Option<String>,
 }
